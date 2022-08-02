83ef1e109b6e78a4c2db075682f9d5309cc5ee16
apple0626 <hj1688@naver.com>
2022-08-02 15:02:26 +0900

    upload 10973 code and guide

---
+++ Brute Force/10973/10973_code.py
@@ -0,0 +1,12 @@
+n = int(input())
+num_list = list(map(int, input().split()))
+for i in range(n - 1, 0, -1):
+    if num_list[i] < num_list[i - 1]:
+        for j in range(n - 1, 0, -1):
+            if num_list[j] < num_list[i - 1]:
+                num_list[j], num_list[i - 1] = num_list[i - 1], num_list[j]
+                num_list = num_list[:i] + sorted(num_list[i:], reverse = True)
+                print(*num_list)
+                exit(0)
+
+print(-1)
