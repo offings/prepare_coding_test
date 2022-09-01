a6fd22494b32d6c71c8fbd7986c93d458a1c1e0b
apple0626 <hj1688@naver.com>
2022-09-01 22:09:59 +0900

    upload 2193 code and guide

---
+++ Dynamic Programming/2193/2193_code.py
@@ -0,0 +1,11 @@
+n = int(input())
+dp = [[0] * 2 for _ in range(91)]
+
+dp[1] = [0, 1]
+dp[2] = [1, 0]
+
+for i in range(3, 91):
+    dp[i][0] += dp[i - 1][0] + dp[i - 1][1]
+    dp[i][1] += dp[i - 1][0]
+
+print(sum(dp[n]))
